9bf1eab312f391725c9a599cc9c21847a0906a5f
Asif-GD <asif.coach@gmail.com>
2026-06-11 10:44:43 +0530

    test skip functionality in range

--- test.py
+++ test.py
@@ -5,50 +5,55 @@ from main import *
 player_list_4 = ["Asif", "Rahul", "Mahesh", "Shiva"]
 player_list_6 = ["Asif", "Rahul", "Mahesh", "Shiva", "Dinesh", "Rajiv"]
 
-print(f"Before shuffle/re-order: {player_list_4}")
-
-# shuffle players using random.shuffle()
-random.shuffle(player_list_4)
-print(f"After shuffle: {player_list_4}")
-
-# reorder players using random.choice()
-player_list_4_copy = player_list_4.copy()
-reordered_player_list = list()
-for count in range(len(player_list_4)):
-    random_player = random.choice(player_list_4_copy)
-    reordered_player_list.append(random_player)
-    player_list_4_copy.remove(random_player)
-
-print(f"After reorder: {reordered_player_list}")
-
-# pairs = []
-# for item in pair_4_players(player_list_4):
-#     print(item)
-# pairs.append(item)
-
-# print(pairs)
-# random.shuffle(player_list_4)
-# print(player_list_4)
-# for item in combinations(player_list_4, 2):
-#     print(item)
-#     pairs.append(item)
+# """
+# checking shuffle()
+# """
+# print(f"Before shuffle/re-order: {player_list_4}")
 #
-# print(pairs)
+# # shuffle players using random.shuffle()
+# random.shuffle(player_list_4)
+# print(f"After shuffle: {player_list_4}")
 
-# for item in combinations(pairs, 2):
-#     print(item)
-
-# map_item = map(random.choice(player_list_4), count())
-# print(map_item)
-
-# print(random.choice(player_list_4))
+# """
+# # reorder players using random.choice()
+# """
+# player_list_4_copy = player_list_4.copy()
+# reordered_player_list = list()
+# for count in range(len(player_list_4)):
+#     random_player = random.choice(player_list_4_copy)
+#     reordered_player_list.append(random_player)
+#     player_list_4_copy.remove(random_player)
+#
+# print(f"After reorder: {reordered_player_list}")
 
 # player_map = dict()
 # print(player_list_4)
+# player_list_4_copy = player_list_4.copy()
 # for count in range(1, len(player_list_4) + 1):
-#     print(f"count: {count}")
-#     player_list_4_copy = player_list_4
-#     player_map[count] = random.choice(player_list_4_copy)
-#     player_list_4_copy.remove(player_map[count])
+#     random_player = random.choice(player_list_4_copy)
+#     player_map[random_player] = count
+#     player_list_4_copy.remove(random_player)
 #
 # print(player_map)
+
+"""
+Checking 6 player combinations
+"""
+# print(f"{player_list_6} \n")
+# six_player_combination = combinations(player_list_6, 2)
+# #  combinations twice doesn't work in a way I want
+# # pairs = combinations(six_player_combination,2)
+# for item in six_player_combination:
+#     print(item)
+
+"""
+testing if list subtraction is possible
+"""
+# list_subtraction = player_list_6 - player_list_4  # -> doesn't work
+# print(list_subtraction)
+
+"""
+testing skip in range()
+"""
+# for i in range(0, 7, 2):
+#     print(i)
