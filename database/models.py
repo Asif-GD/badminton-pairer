ad68e9d820008251c350ec8c4f81a6e97ea53ea0
Asif-GD <asif.coach@gmail.com>
2026-08-17 12:46:16 +0530

    made changes to the pairs table

--- database/models.py
+++ database/models.py
@@ -1,10 +1,13 @@
 """
 Models serve as a blueprint of the table in the database.
 """
-from sqlalchemy import String
+
+from sqlalchemy import String, PickleType
+from sqlalchemy.ext.mutable import MutableList
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
+# SQLAlchemy 2.0 style
 class Base(DeclarativeBase):
     pass
 
@@ -15,4 +18,8 @@ class Pair(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True, autoincrement=True)
     session_id: Mapped[str] = mapped_column(String(160), unique=True)
 
-    benched_player: Mapped[str] = mapped_column(String(80))
+    player_list: Mapped[list] = mapped_column(MutableList.as_mutable(PickleType), default=[])
+
+    benched_player: Mapped[list] = mapped_column(MutableList.as_mutable(PickleType), default=[])
+    lucky_player: Mapped[list] = mapped_column(MutableList.as_mutable(PickleType), default=[])
+    seventh_player: Mapped[str] = mapped_column(String(80), default="")
